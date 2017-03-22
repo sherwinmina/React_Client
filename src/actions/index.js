90525cd58ec8f12c66f00636b98e476f24979765
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-22 01:00:52 -0700

    modified files

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
 
 
-export function signinUser({email, password }) {
-  
+export function signinUser({ email, password }) {
+  return function(dispatch) {
+    
+  }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Router, Route, IndexRoute, browserHistory} from 'react-router'
+import { Router, Route, IndexRoute, browserHistory} from 'react-router';
+import reduxThunk from 'redux-thunk';
+
 
 import App from './components/app';
 import Signin from './components/auth/signin';
