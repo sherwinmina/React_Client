f257ee3ee474cbb22d4306007af8981a56668b66
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-07 15:30:01 -0700

    added signup component

---
+++ src/components/auth/signup.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
+
+i
+class Signup extends Component {
+  render () {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Signup
