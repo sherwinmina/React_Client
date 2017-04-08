e68f58409b4ac5aadee597b7249979038088b0ba
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-08 10:34:09 -0700

    check token before react dom render

--- src/index.js
+++ src/index.js
@@ -14,11 +14,20 @@ import Feature from './components//feature';
 import RequireAuth from './components/auth/require_auth';
 import Welcome from './components//welcome';
 import reducers from './reducers';
+import AUTH_USER from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+// If we have a token, consider the user to be signed in
+if (token) {
+  // we need to update application state
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component ={App}>
         <IndexRoute path="welcome" component={Welcome}/>
