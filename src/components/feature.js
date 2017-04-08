724b53ca94391cbd540616c5d03433db6ed4d71f
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-08 10:17:38 -0700

    updated

---
+++ src/components/feature.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class componentName extends Component {
+  render () {
+    return (
+      <div>
+        This is a feature
+      </div>
+    )
+  }
+}
+
+export default componentName
--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ import App from './components/app';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
+import Feature from './components//feature';
+import RequireAuth from './components/auth/require_auth';
+import Welcome from './components//welcome';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
@@ -18,9 +21,11 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path="/" component ={App}>
+        <IndexRoute path="welcome" component={Welcome}/>
         <Route path="signin" component={Signin}/>
-        <Route path="signout" component={Signout}/>
-        <Route path="signup" component={Signup}/>
+        <Route path="signout" component={Signout} />
+        <Route path="signup" component={Signup} />
+        <Route path="feature" component={requireAuth(Feature)} />
       </Route>
     </Router>
   </Provider>
